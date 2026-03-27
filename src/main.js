8c3a34d034488d94ef17e8baff6b23992ca72b27
Hans van Dam <hans@damsoft.nl>
2026-03-27 10:58:50 +0100

    some error fixes

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ new App(document.getElementById('app'));
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register(import.meta.env.BASE_URL + 'sw.js');
   });
 }
